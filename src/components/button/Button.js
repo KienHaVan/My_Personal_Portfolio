e24d72c5f3ed099c6cf949770269fa92203cf5e5
KienHaVan <hakien351998@gmail.com>
2023-01-19 11:16:47 +0700

    animation3

--- src/components/button/Button.js
+++ src/components/button/Button.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Images } from "../../assets";
-import Lottie from "react-lottie";
+// import Lottie from "react-lottie";
+import Lottie from "react-lottie-player";
 
 function Button({
   title = "Contact Me",
@@ -8,19 +9,26 @@ function Button({
   onClick = () => {},
   size = 60,
 }) {
-  const defaultOptions = {
-    loop: true,
-    autoplay: true,
-    animationData: icon,
-    rendererSettings: {
-      preserveAspectRatio: "xMidYMid slice",
-    },
-  };
+  // const defaultOptions = {
+  //   loop: true,
+  //   autoplay: true,
+  //   animationData: icon,
+  //   rendererSettings: {
+  //     preserveAspectRatio: "xMidYMid slice",
+  //   },
+  // };
   return (
     <div className="btn-primary" onClick={onClick}>
       <p className="flex-shrink-0">{title}</p>
       {/* <img src={icon} className="w-[24px] h-[24px]" alt="" /> */}
-      <Lottie options={defaultOptions} height={size} width={size} />
+      {/* <Lottie options={defaultOptions} height={size} width={size} />
+       */}
+      <Lottie
+        loop
+        animationData={icon}
+        play
+        style={{ width: 150, height: 150 }}
+      />
     </div>
   );
 }
